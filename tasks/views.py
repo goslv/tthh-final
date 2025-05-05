3c6577c201e362ac16df399d099f261a432d42bc
Usuario <rvvillalba@fctunca.edu.py>
2025-05-05 16:15:12 -0300

    CRUD del formulario PerfilFuncionarios, Create y listado de los formularios TipoDocumento, Create listado y almacenamiento de los documentos para el formulario DocumentoFuncionario

---
+++ tasks/views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import PerfilFuncionario
+from django.contrib import messages
+from .models import TipoDocumento, DocumentoFuncionario
+from .forms import TipoDocumentoForm, DocumentoFuncionarioForm
+from django.shortcuts import render
+
+def menu_inicio(request):
+    return render(request, 'tasks/menu_inicio.html')
+
+
+def lista_funcionarios(request):
+    funcionarios = PerfilFuncionario.objects.all()
+    return render(request, 'tasks/lista_funcionarios.html', {'funcionarios': funcionarios})
+
+def crear_funcionario(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        cedula = request.POST.get('cedula')
+
+        cargo = request.POST.get('cargo')
+        otro_cargo = request.POST.get('otro_cargo')
+        if cargo == 'Otro' and otro_cargo:
+            cargo = otro_cargo
+
+        departamento = request.POST.get('departamento')
+        otro_departamento = request.POST.get('otro_departamento')
+        if departamento == 'Otro' and otro_departamento:
+            departamento = otro_departamento
+
+        fecha_ingreso = request.POST.get('fecha_ingreso')
+        estado = request.POST.get('estado')
+        estado_civil = request.POST.get('estado_civil')
+
+        funcionario = PerfilFuncionario(
+            nombre=nombre,
+            apellido=apellido,
+            cedula=cedula,
+            cargo=cargo,
+            fecha_ingreso=fecha_ingreso,
+            departamento=departamento,
+            estado=estado,
+            estado_civil=estado_civil
+        )
+        funcionario.save()
+
+        messages.success(request, '¡Funcionario creado exitosamente! ✅')
+        return redirect('lista_funcionarios')
+
+    return render(request, 'tasks/crear_funcionario.html')
+
+def actualizar_funcionario(request, id_funcionario):
+    funcionario = get_object_or_404(PerfilFuncionario, id_funcionario=id_funcionario)
+
+    if request.method == 'POST':
+        funcionario.nombre = request.POST.get('nombre')
+        funcionario.apellido = request.POST.get('apellido')
+        funcionario.cedula = request.POST.get('cedula')
+
+        cargo = request.POST.get('cargo')
+        otro_cargo = request.POST.get('otro_cargo')
+        if cargo == 'Otro' and otro_cargo:
+            funcionario.cargo = otro_cargo
+        else:
+            funcionario.cargo = cargo
+
+        departamento = request.POST.get('departamento')
+        otro_departamento = request.POST.get('otro_departamento')
+        if departamento == 'Otro' and otro_departamento:
+            funcionario.departamento = otro_departamento
+        else:
+            funcionario.departamento = departamento
+
+        funcionario.fecha_ingreso = request.POST.get('fecha_ingreso')
+        funcionario.estado = request.POST.get('estado')
+        funcionario.estado_civil = request.POST.get('estado_civil')
+
+        funcionario.save()
+
+        messages.success(request, '¡Funcionario actualizado exitosamente! ✏️')
+        return redirect('lista_funcionarios')
+
+    return render(request, 'tasks/actualizar_funcionario.html', {'funcionario': funcionario})
+
+def eliminar_funcionario(request, id_funcionario):
+    funcionario = get_object_or_404(PerfilFuncionario, id_funcionario=id_funcionario)
+    funcionario.delete()
+
+    messages.success(request, '¡Funcionario eliminado exitosamente! 🗑️')
+    return redirect('lista_funcionarios')
+
+# ---------------- TIPO DOCUMENTO ----------------
+
+def lista_tipos_documento(request):
+    tipos = TipoDocumento.objects.all()
+    return render(request, 'tasks/lista_tipos_documento.html', {'tipos': tipos})
+
+def crear_tipo_documento(request):
+    if request.method == 'POST':
+        form = TipoDocumentoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_tipos_documento')
+    else:
+        form = TipoDocumentoForm()
+    return render(request, 'tasks/crear_tipo_documento.html', {'form': form})
+
+# ---------------- DOCUMENTOS FUNCIONARIOS ----------------
+
+def lista_documentos(request):
+    documentos = DocumentoFuncionario.objects.select_related('id_funcionario', 'id_tipo_documento').all()
+    return render(request, 'tasks/lista_documentos.html', {'documentos': documentos})
+
+
+
+def crear_documento(request):
+    if request.method == 'POST':
+        form = DocumentoFuncionarioForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_documentos_funcionario')
+    else:
+        form = DocumentoFuncionarioForm()
+    return render(request, 'tasks/crear_documento.html', {'form': form})
